4d1172860ad5dbb9b3e80e85b6fb0d6109f3f396
Prerna Mishra <mishraprerna218@gmail.com>
2023-01-14 21:56:10 +0530

    Array
    link: https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/description/

---
+++ 1725. Number Of Rectangles That Can Form The Largest Square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        int max_len=min(rectangles[0][0],rectangles[0][1]),c=1;
+        for(int i=1;i<rectangles.size();i++){
+            int min_len= min(rectangles[i][0],rectangles[i][1]);
+            if(min_len==max_len){
+                c++;
+            }else if(min_len>max_len){
+                max_len=min_len;
+                c=1;
+            }
+        }
+        return c;
+    }
+};
